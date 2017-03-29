61a869efce5d5961fcc18368eac938be32dec862
ltran6 <Leland_Tran@Intuit.com>
2017-03-29 11:43:09 -0700

    Added basic custom MovieCell with overview - walkthrough part 3

---
+++ LTCPFlicks/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  LTCPFlicks
+//
+//  Created by Tran, Leland on 3/29/17.
+//  Copyright © 2017 Tran, Leland. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
